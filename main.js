fa4e9df96e849a674ddd25ec001a92340377a9a0
OpedHoped <OpedHoped@yandex.ru>
2024-06-03 11:22:49 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,14 @@ const corsOptions = {
 };
 
 const app = express()
-app.use(bodyParser.json())
-app.use(cors(corsOptions))
+// app.use(bodyParser.json())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
+// app.use(cors(corsOptions))
 app.post('/db', (req, res)=> {
     const db = JSON.parse(fs.readFileSync("db.json", "utf-8"))
     console.log(req.body)
