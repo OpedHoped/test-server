464e342a1dadcc5e9780774e79f163fc38d631ec
OpedHoped <OpedHoped@yandex.ru>
2024-06-03 10:44:58 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const fs = require("fs")
 
 const corsOptions = {
 	origin: '*',
@@ -9,12 +10,13 @@ const corsOptions = {
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors(corsOptions))
 app.post('/db', (req, res)=> {
     res.send({"status":"success", "body":req.body})
 })
 
 app.get('/', (req, res)=> {
-    res.send("server live")
+    res.send(fs.readFileSync("index.html", "utf-8"))
 })
 
 app.listen(5155, ()=>  {
