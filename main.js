7ed00479c382f2a3b41adffc24202dea62f9af51
OpedHoped <OpedHoped@yandex.ru>
2024-06-05 01:54:58 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const fs = require("fs")
 const PORT = 5549
 
+const authPass = "@b4tib-an0Zad2_" //пароль для входа
+
 const corsOptions = {
 	origin: '*',
     methods: '*'
@@ -23,27 +25,37 @@ app.get('/', (req, res)=> {
     res.send(fs.readFileSync("index.html", "utf-8"));
 })
 app.get("/db", (req,res)=> {
-    res.send(fs.readFileSync("db.json", "utf-8"));
+    res.send(fs.readFileSync("./json/db.json", "utf-8"));
 })
 app.get("/images", (req, res)=> {
-    res.send(fs.readFileSync("images.json", "utf-8"));
+    res.send(fs.readFileSync("./json/images.json", "utf-8"));
 })
 app.get("/users", (req, res)=> {
     res.send(fs.readFileSync("admin.html", "utf-8"));
 })
 app.get("/src/users", (req, res)=> {
-    res.send(fs.readFileSync("users.json", "utf-8"));
+    res.send(fs.readFileSync("./json/users.json", "utf-8"));
 })
-app.post('/src/users', (req, res)=> {
+app.post("/login", (req, res)=> {
+    res.send({"data":req.body.data == authPass})
+})
+app.post("/src/users", (req, res)=> {
     if (req.query.save) {
-        fs.writeFileSync("users.json", JSON.stringify(req.body));
+        if (req.body.pass == authPass) {
+            fs.writeFileSync("./json/users.json", JSON.stringify(req.body.data));
+            res.send({"status":"Успешно"});
+        } else {
+            res.send({"status":"ты чем там занимаешься?! Щас ис клана кикну!"});
+        }
     } else {
-        const users = Array.from(JSON.parse(fs.readFileSync("users.json", "utf-8")));
+        const users = Array.from(JSON.parse(fs.readFileSync("./json/users.json", "utf-8")));
         users.push(req.body);
-        fs.writeFileSync("users.json", JSON.stringify(users));
+        fs.writeFileSync("./json/users.json", JSON.stringify(users));
+        res.send({"status":"Успешно"});
     }
-    res.send({"status":"sucess"});
+    
 })
+
 
 
 app.listen(PORT, ()=>  {
