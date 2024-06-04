0b03cdbf233316b97d22cca23c927a467ad170b2
OpedHoped <OpedHoped@yandex.ru>
2024-06-04 03:45:56 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs")
+const PORT = 5549
 
 const corsOptions = {
 	origin: '*',
@@ -10,31 +11,33 @@ const corsOptions = {
 
 const app = express()
 app.use(bodyParser.json())
+// app.use(corsOptions.json())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
-app.post('/', (req, res)=> {
-    const db = Array.from(JSON.parse(fs.readFileSync("db.json", "utf-8")))
-    db.push(req.body)
-    fs.writeFileSync("db.json", JSON.stringify(db))
-    res.send({"data":db})
-})
-app.post("/db", (req,res)=> {
-    res.send("success")
-})
+
 app.get('/', (req, res)=> {
-    res.send(fs.readFileSync("index.html", "utf-8"))
+    res.send(fs.readFileSync("index.html", "utf-8"));
 })
-app.get('/db', (req, res)=> {
-    res.send(fs.readFileSync("db.json", "utf-8"))
+app.get("/db", (req,res)=> {
+    res.send(fs.readFileSync("db.json", "utf-8"));
 })
 app.get("/images", (req, res)=> {
-    res.send(fs.readFileSync("images.json", "utf-8"))
+    res.send(fs.readFileSync("images.json", "utf-8"));
+})
+app.get("/users", (req, res)=> {
+    res.send(fs.readFileSync("users.json", "utf-8"));
+})
+app.post('/users', (req, res)=> {
+    const users = Array.from(JSON.parse(fs.readFileSync("users.json", "utf-8")));
+    users.push(req.body);
+    fs.writeFileSync("users.json", JSON.stringify(users));
+    res.send({"data":users});
 })
 
-app.listen(5155, ()=>  {
-    console.log("server start on address http://127.0.0.1:5155")
+app.listen(PORT, ()=>  {
+    console.log(`server start on address http://127.0.0.1:${PORT}`);
 })
